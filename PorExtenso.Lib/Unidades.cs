b9559573c8e9fc5829b04343db4902163adf8c8c
jersonb <jersonlbrito@hotmail.com>
2020-10-05 08:58:06 -0300

    simplificando unidades

--- PorExtenso.Lib/Unidades.cs
+++ PorExtenso.Lib/Unidades.cs
@@ -1,23 +1,21 @@
-using System.Collections.Generic;
-
 namespace PorExtenso.Lib
 {
     public class Unidades : Base
     {
         protected override string EscreveDescricao(int valor)
-            => new Dictionary<int, string>
-            {
-                {0, "zero" },
-                {  1, "um"},
-                {  2, "dois"},
-                {  3, "três"},
-                {  4, "quatro"},
-                {  5, "cinco"},
-                {  6, "seis"},
-                {  7, "sete"},
-                {  8, "oito"},
-                {  9, "nove"},
-                { 10, "dez"}
-            }[valor];
+            => new string[]
+            { 
+                "zero"
+                , "um"
+                , "dois"
+                , "três"
+                , "quatro"
+                , "cinco"
+                , "seis"
+                , "sete"
+                , "oito"
+                , "nove"
+                , "dez" 
+                }[valor];
     }
 }
